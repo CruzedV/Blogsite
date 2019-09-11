48646efa235b88a4f1e78c727faa61baafae73b6
CruzedV <48482781+CruzedV@users.noreply.github.com>
2019-09-11 18:40:17 +0300

    Start
    I'm load the first version of project

---
+++ app/blogsite/blog/views.py
@@ -0,0 +1,110 @@
+from django.core.paginator import Paginator
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+
+from blog.models import Tag
+from blog.models import Post
+from blog.utils import ObjectDetailMixin
+from blog.utils import ObjectCreateMixin
+from blog.utils import ObjectUpdateMixin
+from blog.utils import ObjectDeleteMixin
+from blog.forms import TagForm
+from blog.forms import PostForm
+from django.db.models import Q
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+    raise_exception = True
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Tag
+    form_model = TagForm
+    template = 'blog/tag_update.html'
+    raise_exception = True
+
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
+
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create.html'
+    raise_exception = True
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Post
+    form_model = PostForm
+    template = 'blog/post_update.html'
+    raise_exception = True
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
+
+
+
+def posts_list(request):
+    search_query = request.GET.get('search', '')
+    if search_query:
+        posts = Post.objects.filter(
+                                Q(title__icontains=search_query) |
+                                Q(body__icontains=search_query)
+                                    )                             
+    else:
+        posts = Post.objects.all()
+
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 3)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+            'page_object': page,
+            'is_paginated': is_paginated,
+            'next_url': next_url,
+            'prev_url': prev_url
+    }
+    return render(request, 'blog/index.html', 
+                context=context)
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', 
+                context={'tags': tags})
+
+
+def test_page(request):
+    int_list = [i for i in range(12)]
+    return render(request, 'blog/test_page.html',
+                context={'int_list': int_list})
