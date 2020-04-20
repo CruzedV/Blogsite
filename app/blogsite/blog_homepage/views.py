d196954382da5cc643f947a85eace63d4cab83d0
cruzedv <mr.dobrikv@mail.ru>
2020-04-20 16:05:17 +0300

    VisualStudioCode update
    Now OuSIite project can be open with VSL

--- app/blogsite/blog_homepage/views.py
+++ app/blogsite/blog_homepage/views.py
@@ -44,9 +44,9 @@ def register(request):
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user = authenticate(username=username, 
-                                password=password
-                                )
+            user = authenticate(
+                            username=username, 
+                            password=password)
             login(request, user)
 
             return redirect('/')
